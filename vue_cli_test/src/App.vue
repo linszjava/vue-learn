008e580e99f8cdcfc119dbf19d6eb887759e691f
linszjava <linszjava@gmail.com>
2023-04-19 05:27:30 +0800

    props配置

--- vue_cli_test/src/App.vue
+++ vue_cli_test/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <School></School>
+    <!-- <School></School> --> 
+    <prop-student name="李清照" :age="22" sex="女"></prop-student> <br/>
+    <prop-student></prop-student> <br/>
+    <prop-student></prop-student>
   </div>
 </template>
 
 <script>
+import PropStudent from './components/PropStudent.vue';
 import School from './components/School.vue';
 export default {
-  components: { School },
+  components: { School, PropStudent },
   name: 'App',
+  data() {
+    return {
+    
+    
+    };
+  },
+  
 
 
 };
